faaa6d7360569f6158d5e49576cc334d706ca28f
linhdt <linh626688@gmail.com>
2019-10-10 15:23:14 +0700

    init commit, learn react udemy

---
+++ src/firebase/test.js
@@ -0,0 +1,7 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+
+
+const firestore = firebase.firestore();
+firestore.collection('user').doc('8FmHpWg9kqepgt9CZey').collection('cartItems').doc('YsjdBTSriIc4EVDC9pq8')
