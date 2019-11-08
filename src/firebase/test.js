bb46819e5e6d97bb303132bbef3560f652a35215
linhdt <linh626688@gmail.com>
2019-11-08 14:58:02 +0700

    init boilerplate

--- src/firebase/test.js
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-import 'firebase/firestore';
-
-
-
-const firestore = firebase.firestore();
-firestore.collection('user').doc('8FmHpWg9kqepgt9CZey').collection('cartItems').doc('YsjdBTSriIc4EVDC9pq8')
